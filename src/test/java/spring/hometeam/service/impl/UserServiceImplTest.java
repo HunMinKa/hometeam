b147b522c7585e65285f7b19b2f0b74dc2a94d3e
SUPREMA\hmka2 <hmka2@suprema.co.kr>
2023-11-04 11:53:11 +0900

    CI/CD 적용

--- src/test/java/spring/hometeam/service/impl/UserServiceImplTest.java
+++ src/test/java/spring/hometeam/service/impl/UserServiceImplTest.java
@@ -21,31 +21,43 @@ import static org.mockito.Mockito.*;
 @SpringBootTest
 class UserServiceImplTest {
 
-
+    // 테스트 대상 클래스의 인스턴스를 생성하고, @Mock 어노테이션으로 생성된 모의 객체를 그 안에 주입합니다.
     @InjectMocks
     private UserServiceImpl userService;
 
+    //주어진 의존성에 대한 모의 구현을 생성하는 데 사용되는 어노테이션
     @Mock
     private UserRepository userRepository;
 
+
     @Before
     public void setUp() {
-        MockitoAnnotations.initMocks(this);
+        MockitoAnnotations.openMocks(this);
+       // 이 메소드는 MockitoAnnotations.initMocks(this);를 사용하여 모의 객체를 초기화합니다. 모의 객체를 userService에 주입하기 위해 준비.
     }
 
     @Test
     public void testCreateUser() {
-        // 모킹된 userRepository를 사용하여 save 메서드를 호출하면 예상된 결과를 반환하도록 설정
+        /*Arrange-Act-Assert (AAA) 패턴
+           Arrange: 테스트를 설정합니다. 사용자 객체를 생성
+           Act: 테스트 대상 메소드를 호출
+           Assert: 결과를 기대값과 비교합니다
+         */
+
+
+        // Arrange: 모킹된 userRepository를 사용하여 save 메서드를 호출하면 예상된 결과를 반환하도록 설정
         User user = new User();
         user.setId(1);
         user.setName("testuser");
 
         when(userRepository.save(any(User.class))).thenReturn(user);
+        // 단위 테스트에서 UserRepository의 save() 메서드가 호출될 때 실제 데이터베이스에 접근하는 대신에, 미리 정의한 User 객체를 반환하도록 설정하여 UserRepository와 상호 작용하는 코드를 테스트할 때 일관된 결과를 얻을 수 있게 하는 것
 
-        // UserService의 createUser 메서드 호출
+
+        // Act: UserService의 createUser 메서드 호출
         User createdUser = userService.createUser(user);
 
-        // 결과 확인
+        // Assert: 결과 확인
         assertEquals(1, createdUser.getId());
         assertEquals("testuser", createdUser.getName());
 
