ea1a32c9ff8fcc0d3867409dbc30c330301055db
XPERIX\hmka2 <hmka2@xperix.com>
2023-12-02 21:12:36 +0900

    no message

---
+++ src/main/java/spring/hometeam/entity/Task.java
@@ -0,0 +1,25 @@
+package spring.hometeam.entity;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Date;
+@Entity
+@Data
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String title;
+    private String description;
+    private int teamId;
+    public Integer parentId;
+    private int completionRate;
+    private Date dueDate;
+    private String priority;
+
+}
